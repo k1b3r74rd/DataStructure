da866ea60ea114fe5bed82ed8b910d907260d44c
k1b3r74rd <kibertard@gmail.com>
2022-11-08 18:23:09 +0700

    Лабораторная 6. Графы

--- lab_6.py
+++ lab_6.py
@@ -1,9 +1,9 @@
 import random
 
-print("Введите количество узлов")
+print("Введите количество узлов: >>>", end=' ')
 N = int(input())
 max_graph = ((N ** 2) - N) // 2
-print("Введите число рёбер от", N - 1, "до", max_graph)
+print("Введите число рёбер от", N - 1, "до", max_graph, ': >>>', end=' ')
 M = int(input())
 
 graph = [[0] * N for i in range(N)]  # Создание матрицы дорог и заполнение главной диагонали
@@ -28,23 +28,34 @@ while True:  # Заполнение матрицы значениями доро
     else:
         continue
 
-for x in range(N):  # Очистка матрицы от лишних значений -1.
-    for y in range(N):
-        if graph[x][y] == -1:
-            graph[x][y] = 0
-
+print('Сгенерированный граф: ')
 for row in graph:  # Вывод матрицы.
     print(row)
 
-print('\n\n\n')
-
-for k in range(N):  # Алгоритм Флойда-Уоршелла
+for k in range(N):  # Меняем -1 на "объездной путь"
     for i in range(N):
         for j in range(N):
-            # if i == j:
-            #     continue
-            if graph[i][j] == 0:
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            if graph[i][j] == -1:
+                graph[i][j] = graph[i][k] + graph[k][j]
 
+print('\nОтредактированный граф: ')
 for row in graph:  # Вывод матрицы.
     print(row)
+
+print('Введите длину дороги: >>>', end=' ')
+option_task = int(input())
+route_is_available = False
+for i in range(N):
+    city_count = 0
+    for j in range(N):
+        if graph[i][j] <= option_task:
+            city_count += 1
+
+    if city_count == N:
+        route_is_available = True
+        break
+
+if route_is_available:
+    print('Такой город ЕСТЬ.')
+else:
+    print('Таких городов НЕТ.')
